925cf1e5b5c9893830f780941dad10cfb924410e
Ujjwal Aglawe <aglaweujjwal@gmail.com>
2025-08-26 21:14:31 +0530

    For Deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,10 @@
-import { useState } from 'react';
-import { createAccount, addAccount } from './utils/wallet';
-import type { WalletAccount } from './utils/wallet';
-import { Navbar } from './components/Navbar';
 import { Routes,Route } from 'react-router-dom';
 import {  Solana } from './components/Solana';
 import { Home } from './components/Home';
 import { Eth } from './components/Eth';
 
 function App() {
-  const [mnemonic, setMnemonic] = useState('');
-  const [accounts, setAccounts] = useState<WalletAccount[]>([]);
-  const [showMnemonic, setShowMnemonic] = useState(false);
-
-  const handleCreateWallet = () => {
-    const { mnemonic, accounts } = createAccount();
-    setMnemonic(mnemonic);
-    setAccounts(accounts);
-  };
-
-  const handleAddAccount = () => {
-    if (!mnemonic) return alert('Create a wallet first!');
-    const newAccount = addAccount(mnemonic, accounts);
-    setAccounts([...accounts, newAccount]);
-  };
+  
 
   return (
     <>
